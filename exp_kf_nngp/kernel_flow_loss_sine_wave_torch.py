2536295aa7e40c759ca1a62250a79eec0a4b362e
Waleed <waleed.binkhalid@hotmail.com>
2022-06-10 15:14:14 +0200

    Added batch norm and different activations to sine wave loss experiment

--- exp_kf_nngp/kernel_flow_loss_sine_wave_torch.py
+++ exp_kf_nngp/kernel_flow_loss_sine_wave_torch.py
@@ -7,10 +7,15 @@ import torch
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
 import random 
+import absl.app
+
 sys.path.insert(0, os.getcwd() + '/.')
 from KernelFlow import KernelFlowsNP
 
+FLAGS = absl.app.flags.FLAGS
+
 def train_network(model, dataloader, epochs, learning_rate, val_set = False):
+    model.train()
     optimizer =  torch.optim.Adam(model.parameters(), lr=learning_rate)
     criterion = nn.MSELoss()
     total_loss = []
@@ -65,13 +70,34 @@ class SineWave(Dataset):
     def __len__(self):
         return self.n_samples
 
-def get_torch_nn(n=1):
+def get_torch_relu_nn(n=1):
+    layers = []
+    for _ in range(n):  # n_layers
+        layers += [
+        nn.Linear(32, 32),
+        nn.ReLU()
+        ]
+        if _%5 == 0 and FLAGS.batch_norm:
+            layers += [nn.BatchNorm1d(32)]
+
+    model = nn.Sequential(
+        nn.Linear(1, 32),
+        nn.ReLU(),
+        *layers,
+        nn.Linear(32, 1),
+    )
+    return model
+
+def get_torch_sigmoid_nn(n=1):
     layers = []
     for _ in range(n):  # n_layers
         layers += [
         nn.Linear(32, 32), 
         nn.Sigmoid()
         ]
+
+    if _%5 == 0 and FLAGS.batch_norm:
+        layers += [nn.BatchNorm1d(32)]
 
     model = nn.Sequential(
         nn.Linear(1, 32),
@@ -85,7 +111,7 @@ def get_mse_loss(pred, y):
     return 0.5* np.mean((pred-y)**2)
 
 def get_loss_from_nn(training_dataset_size_arr, depth_arr):
-    print("Evaluating Neural Networks...")
+    print("Evaluating Neural Networks with " + FLAGS.activation + " activation...")
     test_size = 100
     val_size = 10
 
@@ -111,15 +137,21 @@ def get_loss_from_nn(training_dataset_size_arr, depth_arr):
         train_dataloader = DataLoader(dataset=train_dataset, batch_size = int(training_size), shuffle=False)
         train_xs, train_ys = next(iter(train_dataloader))
 
-        for n in tqdm(depth_arr):#, 2, 3, 5, 10]):
+        for n in tqdm(depth_arr):
             network_depth = n
-            net = get_torch_nn(network_depth)
+            if FLAGS.activation == 'relu':
+                net = get_torch_relu_nn(network_depth)
+            elif FLAGS.activation == 'sigmoid':
+                net = get_torch_sigmoid_nn(network_depth)
+            else:
+                raise Exception("Please select relu or sigmoid as activation layers")
             learning_rate = 0.001
-            training_steps = 5000
+            training_steps = 10000
             train_network(model=net, dataloader=train_dataloader, 
                             epochs=training_steps, learning_rate=learning_rate, val_set=False)
 
             with torch.no_grad():
+                net.eval()
                 predict_nn = net(test_xs)
             ax.plot(test_xs, predict_nn)
             ax.plot(test_xs, test_ys)
@@ -157,19 +189,12 @@ def get_loss_from_kf_rbf(training_dataset_size_arr):
         KF_rbf = KernelFlowsNP(kernel_name, mu)
         iterations = 1000
         batch_size = training_size
-        X_train_perturbed_rbf = KF_rbf.fit(train_xs, train_ys, iterations, batch_size = batch_size, learning_rate=0.01, reg=0.0001)
+        X_train_perturbed_rbf = KF_rbf.fit(train_xs, train_ys, iterations, batch_size = batch_size, learning_rate=0.01, reg=0.0001, type_epsilon="usual")
         predict_kf_rbf = KF_rbf.predict(test_xs, regu=0.0001)
         batch_size_vs_loss[str(training_size)] = get_mse_loss(pred=predict_kf_rbf, y=test_ys)
     return batch_size_vs_loss
 
-if __name__ == "__main__":
-    training_dataset_size_arr = np.arange(5, 11, 1, dtype=int)
-    depth_arr = [0, 1, 2, 3, 5, 8, 11, 14]
-    depth_vs_test_loss_nn = get_loss_from_nn(training_dataset_size_arr, depth_arr)
-    batch_size_vs_loss_kf_rbf = get_loss_from_kf_rbf(training_dataset_size_arr)
-
-
-    # Plotting MSE vs Network Depth
+def plot_results(training_dataset_size_arr: np.ndarray, depth_arr:np.ndarray, depth_vs_test_loss_nn: dict, batch_size_vs_loss_kf_rbf: dict):
     graph_data = {}
     fig, ax = plt.subplots(1,1)
     for i, training_size in enumerate(training_dataset_size_arr):
@@ -177,26 +202,62 @@ if __name__ == "__main__":
         for key, value in depth_vs_test_loss_nn.items():
             graph_data[training_size] += [value[i]]
 
+    # Plotting MSE vs Network Depth
     for key, value in graph_data.items():
-        ax.plot(value, 'o-', label=f"""Number of Training points = {key}""")
-    x_tick_labels = ['Kernel Flow RBF'] +  ["Depth = " + str(x + 1) for x in depth_arr]
+        ax.semilogy(value, 'o-', label=f"""Number of Training points = {key}""")
+    x_tick_labels = ['Kernel Flow RBF'] +  ["NN Depth = " + str(x + 1) for x in depth_arr]
     ax.set_xticks(np.arange(len(x_tick_labels)))
     ax.set_xticklabels(x_tick_labels, rotation = 45)
     ax.set_xlabel("Architecture")
-    ax.set_ylabel("MSE Loss")
-    plt.legend()
+    ax.set_ylabel("MSE Loss - Log scale")
+    plt.legend(prop={'size': 6})
     plt.tight_layout()
-    fig.savefig("figs/sine_wave_training_mse_loss_vs_arch_sigmoid_torch.png")
+    fig.savefig("figs/sine_wave_training_mse_loss_vs_arch_" + FLAGS.activation +"_torch.png")
+
+def main(_):
+    training_dataset_size_arr = np.arange(5, 11, 1, dtype=int)
+    depth_arr = [0, 1, 2, 5, 10, 15, 20, 25, 30, 35]
+    depth_vs_test_loss_nn = get_loss_from_nn(training_dataset_size_arr, depth_arr)
+    batch_size_vs_loss_kf_rbf = get_loss_from_kf_rbf(training_dataset_size_arr)
+    plot_results(training_dataset_size_arr, depth_arr, depth_vs_test_loss_nn, batch_size_vs_loss_kf_rbf)
+
+if __name__ == "__main__":
+    f = absl.app.flags
+    f.DEFINE_string("activation", "relu",
+                    "which activation to choose")
+    f.DEFINE_bool("batch_norm", False,
+                "To apply batch normalization or not")
+    absl.app.run(main)
+
+
+    # Plotting MSE vs Network Depth
+    # graph_data = {}
+    # fig, ax = plt.subplots(1,1)
+    # for i, training_size in enumerate(training_dataset_size_arr):
+    #     graph_data[training_size] = [batch_size_vs_loss_kf_rbf[str(training_size)]]
+    #     for key, value in depth_vs_test_loss_nn.items():
+    #         graph_data[training_size] += [value[i]]
+
+    # for key, value in graph_data.items():
+    #     ax.plot(value, 'o-', label=f"""Number of Training points = {key}""")
+    # x_tick_labels = ['Kernel Flow RBF'] +  ["Depth = " + str(x + 1) for x in depth_arr]
+    # ax.set_xticks(np.arange(len(x_tick_labels)))
+    # ax.set_xticklabels(x_tick_labels, rotation = 45)
+    # ax.set_xlabel("Architecture")
+    # ax.set_ylabel("MSE Loss")
+    # plt.legend()
+    # plt.tight_layout()
+    # fig.savefig("figs/sine_wave_training_mse_loss_vs_arch_relu_torch.png")
 
 
     # Plotting MSE vs Training size
-    fig, ax = plt.subplots(1,1)
-    for key, value in depth_vs_test_loss_nn.items():
-        ax.plot(training_dataset_size_arr, depth_vs_test_loss_nn[key], 'o-', label=f""" NN Depth {str(key+1)}""")
+    # fig, ax = plt.subplots(1,1)
+    # for key, value in depth_vs_test_loss_nn.items():
+    #     ax.plot(training_dataset_size_arr, depth_vs_test_loss_nn[key], 'o-', label=f""" NN Depth {str(key+1)}""")
 
-    ax.plot(training_dataset_size_arr, batch_size_vs_loss_kf_rbf.values(), '*--', label='Kernel Flow - RBF')
-    ax.set_xlabel("Training data size")
-    ax.set_ylabel("MSE Loss")
-    plt.legend()
-    plt.tight_layout()
-    fig.savefig("figs/sine_wave_training_mse_loss_sigmoid_torch.png")
+    # ax.plot(training_dataset_size_arr, batch_size_vs_loss_kf_rbf.values(), '*--', label='Kernel Flow - RBF')
+    # ax.set_xlabel("Training data size")
+    # ax.set_ylabel("MSE Loss")
+    # plt.legend()
+    # plt.tight_layout()
+    # fig.savefig("figs/sine_wave_training_mse_loss_relu_torch.png")
