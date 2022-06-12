165066d272016a54a4e891cf8f0a3267d2d3f51e
Waleed <waleed.binkhalid@hotmail.com>
2022-06-12 09:16:49 +0200

    Updated bayesian optimization experiment to show band of accuracies for randomly initialized kernels

--- KernelFlow/__init__.py
+++ KernelFlow/__init__.py
@@ -2,5 +2,8 @@ from .Parametric.Frechet.KF_parametric_categorical import *
 from .Torch.KF_parametric_catagorical_torch import *
 from .Non_Parametric.Frechet.KF_NP_frechet import *
 from .Non_Parametric.Autograd.KF_NP_autograd import *
-from .JAX.KF_non_parametric_JAX import *
+try:
+    from .JAX.KF_non_parametric_JAX import *
+except:
+    warnings.warn("Could not import jax. Please install. Remaining modules are imported.")
 from .Non_Parametric.Frechet.kernel_functions import *
--- exp_kf_cnngp/kernel_flow_bayesian_optimization.py
+++ exp_kf_cnngp/kernel_flow_bayesian_optimization.py
@@ -7,11 +7,11 @@ import absl.app
 import sys
 import os
 from skopt.plots import plot_convergence
-    
+import time
 sys.path.insert(0, os.getcwd() + '/.')
 
 from utils import get_dataset, get_label_from_probability
-from KernelFlow import KernelFlowsTorch
+from KernelFlow.Torch.KF_parametric_catagorical_torch import KernelFlowsTorch
 from configs import kernel_flow_configs
 
 FLAGS = absl.app.flags.FLAGS
@@ -20,31 +20,50 @@ DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 def main(_):
     X_train, Y_train, X_test, Y_test = get_dataset(dataset=FLAGS.dataset, train_size=50000, val_size=1000, device=DEVICE)
-    cnn_gp = kernel_flow_configs.get_CNNGP(model_name = FLAGS.CNNGP_model, device=DEVICE)
 
     N_i_arr = np.arange(100, 1600, 100)
-    rand_acc = []
 
+    rand_acc_matrix = []
     # Getting accuracy for randomly initialized CNNGP
-    for N_i in tqdm(N_i_arr):
-        Y_predictions_rand_cnngp = KernelFlowsTorch.kernel_regression(X_test=X_test, X_train=X_train[:N_i], 
-                                                                                    Y_train=Y_train[:N_i], kernel=cnn_gp, 
-                                                                                    regularization_lambda=0.0001, blocksize=250, 
-                                                                                    device=DEVICE)
+    if FLAGS.CNNGP_model == 'alonso_etal_covnet': 
+        rand_acc = []
+        for N_i in tqdm(N_i_arr):
+            Y_predictions_rand_cnngp = KernelFlowsTorch.kernel_regression(X_test=X_test, X_train=X_train[:N_i], 
+                                                                                        Y_train=Y_train[:N_i], kernel=cnn_gp, 
+                                                                                        regularization_lambda=0.0001, blocksize=250, 
+                                                                                        device=DEVICE)
 
-        Y_predictions_rand_cnngp_labels = get_label_from_probability(Y_predictions_rand_cnngp)
-        rand_acc.append(accuracy_score(Y_predictions_rand_cnngp_labels, Y_test.cpu().numpy()) * 100)
+            Y_predictions_rand_cnngp_labels = get_label_from_probability(Y_predictions_rand_cnngp)
+            rand_acc.append(accuracy_score(Y_predictions_rand_cnngp_labels, Y_test.cpu().numpy()) * 100)
 
-    plt.plot(rand_acc)
-    plt.ylim((0,100))
-    plt.show()
+    else:
+        for i in range(5):
+            cnn_gp = kernel_flow_configs.get_CNNGP(model_name = FLAGS.CNNGP_model, device=DEVICE)
+            rand_acc = []
+            for N_i in tqdm(N_i_arr):
+                Y_predictions_rand_cnngp = KernelFlowsTorch.kernel_regression(X_test=X_test, X_train=X_train[:N_i], 
+                                                                                            Y_train=Y_train[:N_i], kernel=cnn_gp, 
+                                                                                            regularization_lambda=0.0001, blocksize=250, 
+                                                                                            device=DEVICE)
+
+                Y_predictions_rand_cnngp_labels = get_label_from_probability(Y_predictions_rand_cnngp)
+                rand_acc.append(accuracy_score(Y_predictions_rand_cnngp_labels, Y_test.cpu().numpy()) * 100)
+            rand_acc_matrix.append(np.array(rand_acc))
+
+        rand_acc_matrix = np.array(rand_acc_matrix)
+        rand_min = rand_acc_matrix.min(axis=0)
+        rand_max = rand_acc_matrix.max(axis=0)
+    # plt.show()
       
     # Training with Bayesian Optimization
     parameter_bounds = [(1e-3, 200.0), (0.0, 200.0)]
     KF_BO = KernelFlowsTorch(cnn_gp, device=DEVICE, regularization_lambda=1e-4)
-    res = KF_BO.fit(X_train, Y_train, iterations=30, batch_size=1200, 
+    start = time.time()
+    res = KF_BO.fit(X_train, Y_train, iterations=50, batch_size=1200, 
                     sample_proportion=0.5, parameter_bounds_BO=parameter_bounds, 
                     random_starts=15, method='bayesian optimization')
+    stop = time.time()
+    print(f"""Bayesian Optimization took {stop - start} seconds to fit for 50 iterations. One iteration took on average {50 / (stop - start)} seconds""")
 
     fig, ax = plt.subplots(1,1)
     plot_convergence(res, ax=ax)
@@ -65,8 +84,10 @@ def main(_):
     if FLAGS.CNNGP_model == 'alonso_etal_covnet':
         ax.plot(N_i_arr, rand_acc, '-*', label='Covnet with parameters from Garriga-Alonso')
     else:
-        ax.plot(N_i_arr, rand_acc, '-*', label='CNNGP with randomly initialized $\sigma_w$ and $\sigma_b$')
+        # ax.plot(N_i_arr, rand_acc, '-*', label='CNNGP with randomly initialized $\sigma_w$ and $\sigma_b$')
+        ax.fill_between(N_i_arr, rand_min, rand_max, alpha=0.25, label='CNNGPs with randomly initialized $\sigma_w$ and $\sigma_b$')
         
+
     ax.plot(N_i_arr, bo_acc, '-o', label='Bayesian Optimization Trained CNNGP')
 
     # If experiment is done with covnet then the covnet model in Alonso et al is also compared
@@ -93,7 +114,7 @@ def main(_):
 if __name__ == '__main__':
     f = absl.app.flags
     f.DEFINE_string("CNNGP_model", "covnet",
-                    "which CNNGP model to test on")
+                    "which CNNGP model to test on. For random covnet use covnet. For Covnet from Alonso et al use alonso_etal_covnet. For simple model use simple.")
     f.DEFINE_string("dataset", "mnist",
                 "which dataset to work with")
     absl.app.run(main)
